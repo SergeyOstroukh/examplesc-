d319994f2ba8ed4c3a086f39efeb7ec644ff8a11
SergeyOstroukh <hardmass3ffect@gmail.com>
2023-01-12 00:05:02 +0300

    решил задачку с гирьками

---
+++ example_7/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 5;
+int b = 2;
+int c = 7;
+int d = 8;
+int e = 1;
+int max = a;
+if(a>max) max = a;
+if(b>max) max = b;
+if(c>max) max = c;
+if(d>max) max = d;
+if(e>max) max = e;
+Console.Write("Макс=");
+Console.Write(max);
