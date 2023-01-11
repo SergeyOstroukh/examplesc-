c80be5a104bb4e4614498f5cc1a4019f43211d6f
SergeyOstroukh <hardmass3ffect@gmail.com>
2023-01-11 23:49:27 +0300

    создал программу права, ложь!

---
+++ example_6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите ваше имя:");
+
+string username = Console.ReadLine();
+
+    if(username.ToLower() == "маша")
+    {
+        Console.WriteLine("Ура!!! Привет МАША");
+    }
+    else
+    {
+        Console.Write("Привет, ");
+        Console.Write(username);
+    }
+  
