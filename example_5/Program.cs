d97fe6065a17e3ae5e896ba78e478aa626b6401a
SergeyOstroukh <hardmass3ffect@gmail.com>
2023-01-11 22:51:36 +0300

    добавил случайное число в задании 3

---
+++ example_5/Program.cs
@@ -0,0 +1,5 @@
+﻿int numberA = new Random().Next(1, 10); // случайное число 1,2,3,4,5...9
+int numberB=new Random().Next(1, 10);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA+numberB);
